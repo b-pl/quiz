3647980ba3862cc6181090104706e0ba3ae6caa7
b-pl <bartoszrydzewski@outlook.com>
2020-03-20 19:41:02 +0100

    feat(endGameComponent): Add endGame component - Number of correct answers is saved in localStorage - Component display playerName, score and no. of correct answers - Added some CSS to Button component

--- webapp/src/components/Endgame/Endgame.jsx
+++ webapp/src/components/Endgame/Endgame.jsx
@@ -1,15 +1,37 @@
 import React from 'react'
 import { A } from 'hookrouter'
+import './Endgame.css'
+import Button from '../Button/Button'
 
 function Endgame() {
   let score = localStorage.getItem('score')
+  let player = localStorage.getItem('playerName')
+  let correctAnswers = localStorage.getItem('correctAnswers')
 
   return(
     <div>
-      <h1>gratz!</h1>
-      <h2>Your score: {score}</h2>
-      <p><A href='/'>HOME</A></p>
+      <div className='endgame_wrapper'>
+        <div className='header'>
+          <span className='small-text'>Congratulations</span> <br />
+          {player}
 
+          <hr className='separator' />
+        </div>
+        <div className='content'>
+          <p className='heading'>Your score:</p>
+          <p className='underline big'>{score}</p>
+          <p className='heading'>Correct answers:</p>
+          <p className='big green'>{correctAnswers}</p>
+          <p className='heading'>Wrong answers:</p>
+          <p className='big red'>{10 - correctAnswers}</p>
+        </div>
+        <div className='btnFooter'>
+          <A href='/categories'>
+            <Button label='Again' styling='greenButton smaller' /></A>
+          <A href='/'>  
+            <Button label='Menu' styling='redButton smaller' /></A>
+        </div>
+      </div>
     </div>
   )
 }
